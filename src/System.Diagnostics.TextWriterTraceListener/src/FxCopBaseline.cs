071733ab6b482b7078a44ad3c7de01dc0d48624d
Andrew Theken <andrew@wildbit.com>
2017-04-18 15:29:11 -0400

    Remove non-relevent files.

--- src/System.Diagnostics.TextWriterTraceListener/src/FxCopBaseline.cs
+++
@@ -1,3 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Microsoft.Reliability", "CA2002:DoNotLockOnObjectsWithWeakIdentity", Scope = "member", Target = "System.Diagnostics.DelimitedListTraceListener.#set_Delimiter(System.String)")]
